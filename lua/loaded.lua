5cb4f436c993cba989b5df15d09b3fa7c478fe31
SMLkaiellis08 <65583319+SMLkaiellis08@users.noreply.github.com>
2021-09-26 14:06:44 +0100

    Create loaded.lua

---
+++ lua/loaded.lua
@@ -0,0 +1,3 @@
+print("Script Has Been loaded Already")
+game.StarterGui:SetCore("SendNotification", {Title="cdn.Magma-mc.net"; Text="Script Has Been loaded Already"; Duration=5;})
+game.Players.LocalPlayer:Kick("Error Can Not Run 2 Scripts: Bark, Plugins(JsLua, Magma's Client, 'HttpGet')")
